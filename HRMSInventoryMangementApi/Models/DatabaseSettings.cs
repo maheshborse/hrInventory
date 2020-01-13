32dda17ed720f9721699258ea0818bc5811d7af1
maheshb <maheshb@ULTP668>
2020-01-13 15:56:41 +0530

    Add api Project

---
+++ HRMSInventoryMangementApi/Models/DatabaseSettings.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMSInventoryMangement.Models
+{
+    public class DatabaseSettings
+    {
+        public string DbServer { get; set; }
+        public string DbName { get; set; } = "IDPTokenizer";
+        public string DbPort { get; set; } = "5433";
+        public string DbUser { get; set; }
+        public string DbPassword { get; set; }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return $"Server={this.DbServer};Database={this.DbName};User ID={this.DbUser};Password={this.DbPassword};Port={Convert.ToInt16(this.DbPort)};Integrated Security=false;Timeout=300;CommandTimeout=2400;Enlist=true";
+            }
+        }
+        public static DatabaseSettings InitializeSettings(IConfiguration configuration)
+        {
+            DatabaseSettings settings = new DatabaseSettings();
+            configuration.Bind("Database", settings);
+
+            return settings;
+        }
+    }
+}
---
+++ HRMSInventoryMangementApi/Services/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using HRMSInventoryMangement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMSInventoryMangement.Services
+{
+    public class DataAccess
+    {
+        private string ConnectionString { get; set; }
+        public DataAccess(DatabaseSettings dbSettings)
+        {
+            ConnectionString = dbSettings.ConnectionString;
+        }
+    }
+}
